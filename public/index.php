9e65772a0ad99f505bb7669106b6ec7e21714fc8
João Magdaleno <joaomagdaleno.14@gmail.com>
2019-05-27 02:53:42 -0300

    .htaccess e config e composer

---
+++ public/index.php
@@ -0,0 +1,6 @@
+<?php
+header("Content-type: text/html; charset=utf-8");
+require_once("../config/config.php");
+require_once("../src/vendor/autoload.php");
+
+echo DIRIMG;
--- src/vendor/composer/autoload_static.php
+++ src/vendor/composer/autoload_static.php
@@ -6,9 +6,41 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit0184a56856dad3bfbad68167b268557f
 {
+    public static $prefixLengthsPsr4 = array (
+        'S' => 
+        array (
+            'Src\\' => 4,
+        ),
+        'P' => 
+        array (
+            'PHPMailer\\PHPMailer\\' => 20,
+        ),
+        'A' => 
+        array (
+            'App\\' => 4,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'Src\\' => 
+        array (
+            0 => __DIR__ . '/../..' . '/',
+        ),
+        'PHPMailer\\PHPMailer\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/phpmailer/phpmailer/src',
+        ),
+        'App\\' => 
+        array (
+            0 => __DIR__ . '/../..' . '/../app',
+        ),
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
+            $loader->prefixLengthsPsr4 = ComposerStaticInit0184a56856dad3bfbad68167b268557f::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit0184a56856dad3bfbad68167b268557f::$prefixDirsPsr4;
 
         }, null, ClassLoader::class);
     }
