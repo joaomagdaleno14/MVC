c8c61871fa86374dc3f33ba2c11dbf40899daae5
João Magdaleno <joaomagdaleno.14@gmail.com>
2019-05-30 23:45:22 -0300

    config

--- config/config.php
+++ config/config.php
@@ -2,9 +2,20 @@
 #Arquivos diretórios raizes
 $PastaInterna="";
 define('DIRPAGE',"http://{$_SERVER['HTTP_HOST']}/{$PastaInterna}");
+if(substr($_SERVER['DOCUMENT_ROOT'],-1)=='/'){ define('DIRREQ',"{$_SERVER['DOCUMENT_ROOT']}{$PastaInterna}");}else{ define('DIRREQ',"{$_SERVER['DOCUMENT_ROOT']}/{$PastaInterna}");}
 
-if(substr($_SERVER['DOCUMENT_ROOT'],-1)=='/'){
-    define('DIRREQ',"{$_SERVER['DOCUMENT_ROOT']}{$PastaInterna}");
-}else{
-    define('DIRREQ',"{$_SERVER['DOCUMENT_ROOT']}/{$PastaInterna}");
-}
+#Diretórios Especificos
+define('DIRADM',DIRPAGE."public/admin/");
+define('DIRAUDIO',DIRPAGE."public/audio/");
+define('DIRCSS',DIRPAGE."public/css/");
+define('DIRDESIGN',DIRPAGE."public/design/");
+define('DIRFONTES',DIRPAGE."public/fontes/");
+define('DIRIMG',DIRPAGE."public/img/");
+define('DIRJS',DIRPAGE."public/js/");
+define('DIRVIDEO',DIRPAGE."public/video/");
+
+#Acesso ao banco de dados
+define('HOST',"localhost");
+define('DB',"sistema");
+define('USER',"root");
+define('PASS',"");
